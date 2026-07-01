5bcf825e9b93055694b23cfa195d06f2b6d5829c
takachin <ty80164@gmail.com>
2026-07-02 00:04:52 +0900

    del

--- 0427/a.py
+++
@@ -1,9 +0,0 @@
-len = int(input())
-s = list(map(int, input().split()))
-sum = 0
-
-for i in range(len):
-  if i % 2 == 0:
-    sum += s[i]
-    
-print(sum)
--- ABC081/a.py
+++
@@ -1,8 +0,0 @@
-nums = list(map(int,input()))
-count = 0
-
-for i in range(len(nums)):
-    if nums[i] == 1:
-        count = count + 1
-
-print(count)
